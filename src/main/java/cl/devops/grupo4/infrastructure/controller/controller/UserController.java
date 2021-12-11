1a506ac300f90de48809db954bc4f65a08f759ea
luismella <luismellareyes@gmail.com>
2021-12-11 10:58:58 -0300

    Metodo delete

--- src/main/java/cl/devops/grupo4/infrastructure/controller/controller/UserController.java
+++ src/main/java/cl/devops/grupo4/infrastructure/controller/controller/UserController.java
@@ -9,10 +9,7 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -29,6 +26,11 @@ public class UserController {
     public ResponseEntity<?> insertUser(@Valid @RequestBody UserData userData) {
         userService.create(userData);
         return new ResponseEntity("", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@Valid @RequestBody UserData userData) {
+
     }
 
 
